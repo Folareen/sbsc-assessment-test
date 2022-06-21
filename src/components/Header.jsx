e81a9fe36a5ebfa7eda4436dc92214d8d8f4274e
Folareen <sakawahab03@gmail.com>
2022-06-21 12:11:39 +0100

    styled header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import {FaRegUserCircle, FaSignOutAlt} from 'react-icons/fa'
 
 const Header = () => {
+
+  const [drop, setDrop] = useState(false)
+
+  const dropdown = () => {
+    setDrop(!drop)
+  }
+
   return (
     <nav>
-      <ul>
+      <ul className='header-nav'>
         <li className="home-nav ">
-          <Link to="/home" >Home</Link> 
+          <Link to="/home" >
+            Home
+          </Link> 
         </li>
-        <li className="my-account-nav ">
-          <Link to="/my-account" >My Account</Link>
-        </li>
-        <li className="users-nav ">
-          <Link to="/users" >Users</Link>
+        <button className='account-dropdown' onClick={dropdown}>
+          <FaRegUserCircle/>
+        </button>
+        <li className="account-nav" style={drop ? {display:"block"} : {display:"none"}}>
+          <ul className='dropdown'>
+            <li className="my-account-nav ">
+              <Link to="/my-account" >My Account</Link>
+            </li>
+            <li className="users-nav ">
+              <Link to="/users" >Users</Link>
+            </li>
+          </ul>
         </li>
         <li className="logout-nav">
-          <Link to="logout" >Logout</Link>
+          <Link to="logout" ><FaSignOutAlt/></Link>
+          <p className="tooltip">
+            Logout
+          </p>
         </li>
       </ul>
     </nav>
