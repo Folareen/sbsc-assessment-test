183c771a0db382f4832408d468a08131180f7e2c
Folareen <sakawahab03@gmail.com>
2022-06-21 12:50:01 +0100

    styled home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,31 +5,38 @@ const Homepage = () => {
   const [location, setLocation] = useState("")
 
 
-  useEffect (
-    () => {
+  // useEffect (
+  //   () => {
 
-    }, []
-  )
+  //   }, []
+  // )
 
-  const getLocation = () => {
-    navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords;
-    });
-  }
+  // const getLocation = () => {
+  //   navigator.geolocation.getCurrentPosition(position => {
+  //     const { latitude, longitude } = position.coords;
+  //   });
+  // }
 
 
 
   return (
-    <div>
+    <div className='home'>
         <Header active={"home"}/>
-        <h3 className="name">
-
-        </h3>
-        <p className="email">
-
-        </p>
-        <img src="" alt="" className="avatar" />
-        <p className="location"></p>
+        <div className="account-details">
+          <h2 className="account-name">
+            Welcome,<span>Wahab</span>
+          </h2>
+          <p className="account-email">
+            sakawahab@gmail.com
+          </p>
+          <img src="https://reqres.in/img/faces/2-image.jpg" alt="" className="account-avatar" />
+          <p className="current-location">
+            Lagos,Nigeria.
+          </p>
+          <p className="account-summary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, cumque. Sequi odit doloremque itaque tempore veniam officiis corrupti fugit ipsam aut optio temporibus quisquam architecto, sed iure modi velit nemo esse fugiat iste. Ducimus, maxime ab? Quis, quasi quisquam? Consequuntur, consequatur rerum eaque a quas placeat. Praesentium, minima et odio inventore cupiditate autem quasi, facilis, consequatur eius aliquid iure error?
+          </p>
+        </div>
     </div>
   )
 }
--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -5,7 +5,19 @@ const MyAccount = () => {
   return (
     <div>
         <Header active={"my-account"}/>
-        MyAccount
+        <div className="my-account-details">
+          <h2 className="my-account-name">
+            Name
+          </h2>
+          <p className="my-account-email">
+            sakawahab@gmail.com
+          </p>
+          <img src="" alt="" className="my-account-avatar" />
+          <p className="my-current-location"></p>
+          <p className="my-account-summary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex iure tenetur cum dolorum accusantium eligendi est doloribus libero deleniti repudiandae.
+          </p>
+        </div>
     </div>
   )
 }
